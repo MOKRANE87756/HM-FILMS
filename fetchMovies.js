1106d1f69bdb14fb1b99c4173cc1c1c2ae1fea36
My shop <HAMDAOUIMOKRANE@GMAIL.COM>
2025-01-18 12:17:08 +0100

    Update fetchMovies.js

--- fetchMovies.js
+++ fetchMovies.js
@@ -1,13 +1,16 @@
 // fetchMovies.js
-// هذا الملف مسؤول عن جلب البيانات من الـ API
+// جلب الأفلام بناءً على الفئة المختارة
 async function fetchMoviesByCategory(category) {
-    const apiKey = 'YOUR_API_KEY';
+    const apiKey = 'curl --request GET \
+     --url 'https://api.themoviedb.org/3/movie/550?append_to_response=images&language=en-US&include_image_language=en,null' \
+     --header 'Authorization: Bearer ACCESS_TOKEN' \
+     --header 'accept: application/json'';  // API key الخاص بـ TMDb
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${category}&language=en-US`;
 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        return data.results;
+        return data.results;  // إرجاع الأفلام المستخلصة من الـ API
     } catch (error) {
         console.error('Error fetching movies:', error);
     }
